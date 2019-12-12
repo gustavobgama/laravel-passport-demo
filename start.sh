18d1120d83d657a211989d585b1302f45053aba3
Gustavo Gama <gustavobgama@gmail.com>
2019-12-12 17:59:17 -0300

    Add the application key generation to start script and refactor a little to execute some commands just once.

--- start.sh
+++ start.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 set -e
 
-echo "===> Generating configurations"
+echo "===> Running composer install"
+composer install --ansi
+
 ENV_FILE=".env"
 if [ ! -f $ENV_FILE ]; then
+    echo "===> Generating configurations"
     cp ".env.example" $ENV_FILE
+
+    echo "===> Generating application key"
+    php artisan key:generate --ansi
+
+    echo "===> Running migrations (with seed)"
+    touch ./database/database.sqlite
+    php artisan migrate --seed --ansi
 fi
 
-echo "===> Running composer install"
-composer install
-
 echo "===> Running migrations"
-touch ./database/database.sqlite
-php artisan migrate --seed || true
+php artisan migrate --ansi
 
 echo "===> Creating keys for passport (only for API)"
-php artisan passport:keys || true
+php artisan passport:keys --ansi || true
 
 echo "===> Configuring storage permissions"
 chown -R www-data:www-data ./storage
 
 echo "===> Running the PHP server"
-php artisan serve --host=0.0.0.0 --port=$1
+php artisan serve --host=0.0.0.0 --port=$1 --ansi
