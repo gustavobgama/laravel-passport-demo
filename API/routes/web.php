06ede2d309a629c1fc5cd26f99eaa4e8bdb4a948
Gustavo Gama <gustavobgama@gmail.com>
2019-12-13 15:32:48 -0300

    Remove unnecessary controller.

--- API/routes/web.php
+++ API/routes/web.php
@@ -12,13 +12,14 @@
 */
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::middleware('auth')->get('/home', function() {
+    return view('home');
+})->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
